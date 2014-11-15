f2bc72e74590abd48bcc9ffac7418cc719c9926e
Marcus Kohnert <marcus.dev@live.com>
2014-11-15 17:26:24 +0100

    Contributors from all saxsys repos are loaded and displayed

--- TypeProviderFiddling/TypeProviderFiddling/TypeProvModule.fs
+++ TypeProviderFiddling/TypeProviderFiddling/TypeProvModule.fs
@@ -5,17 +5,11 @@ open FSharp.Data
     type saxSysRepo = JsonProvider<"https://api.github.com/users/saxsys/repos">
 
     type contributorDoc = JsonProvider<"https://api.github.com/repos/saxsys/CinemaKata/contributors">
-
-    let GetContributors url:string = 
-        contributorDoc.Load url
-        |> Seq.iter (fun c -> printfn "%s" (c.ToString()))
-
+    
     let Get() =
         saxSysRepo.Load "https://api.github.com/users/saxsys/repos"
-        |> Seq.iter (fun r -> printfn "Name: %s" r.Name)
-
-        saxSysRepo.Load "https://api.github.com/users/saxsys/repos"
-        |> Seq.iter (fun r -> contributorDoc.Load r.ContributorsUrl)
-
-//        contributorDoc.Load "https://api.github.com/repos/saxsys/CinemaKata/contributors"
-//        |> Seq.iter (fun r -> printfn "Name: %s" r.Login)
+        |> Seq.map (fun repo -> repo.ContributorsUrl)
+        |> Seq.collect (fun url -> contributorDoc.Load url)
+        |> Seq.map (fun contributor -> contributor.Login)
+        |> Seq.distinct
+        |> Seq.iter (fun c -> printfn "%s" c)
