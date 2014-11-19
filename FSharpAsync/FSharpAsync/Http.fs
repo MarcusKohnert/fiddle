d92252cf5386e33df5c6b077af0a3687cbec6524
Marcus Kohnert <marcus.dev@live.com>
2014-11-19 17:49:34 +0100

    Use AsyncGetResponse

--- FSharpAsync/FSharpAsync/Http.fs
+++ FSharpAsync/FSharpAsync/Http.fs
@@ -6,7 +6,7 @@ open System.IO
 let Get(url:string) =
     async {
         let request = WebRequest.CreateHttp(url)
-        let! response = Async.AwaitTask(request.GetResponseAsync())
+        let! response = request.AsyncGetResponse()
         printfn "Got response from %s" url
         let stream = response.GetResponseStream()
         use reader = new StreamReader(stream)
