e401bd4330ed25914d4b3fe0ae6f43bd0b628c16
Marcus Kohnert <marcus.dev@live.com>
2014-11-24 13:38:50 +0100

    Added HtmlProviderSample

---
+++ NugetStats/NugetStats/Program.fs
@@ -0,0 +1,8 @@
+﻿[<EntryPoint>]
+let main args = 
+    
+    do System.Net.WebRequest.DefaultWebProxy.Credentials <- System.Net.CredentialCache.DefaultNetworkCredentials
+    Stats.GetStats()
+
+    System.Console.ReadLine() |> ignore
+    0 // return an integer exit code
---
+++ NugetStats/NugetStats/Stats.fs
@@ -0,0 +1,13 @@
+﻿module Stats
+
+open FSharp.Data
+
+type NugetProfile = HtmlProvider<"https://www.nuget.org/profiles/MarcusKohnert/">
+
+let GetStats() =
+    let downloads = NugetProfile().Html
+                                  .Descendants(fun n -> n.HasId("stats"))
+                                  |> Seq.collect (fun n -> 
+                                                      n.Descendants(fun descendants -> descendants.HasClass("stat-number")))
+                                  |> Seq.last
+    printfn "%s" <| downloads.InnerText()
