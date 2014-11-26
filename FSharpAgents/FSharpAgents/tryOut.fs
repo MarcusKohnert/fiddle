b656989ab74ea7c940c3418bf6f9a2648b62a37b
Marcus Kohnert <marcus.dev@live.com>
2014-11-26 22:31:19 +0100

    Added FSharpAgents

---
+++ FSharpAgents/FSharpAgents/tryOut.fs
@@ -0,0 +1,27 @@
+﻿module tryOut
+
+type Message =
+| TxtMessage of string
+| ReplyMessage of AsyncReplyChannel<string>
+
+let agent = MailboxProcessor.Start(fun inbox ->
+    let rec messageLoop = async {
+        let! msg = inbox.Receive()
+        match msg with
+        | TxtMessage m -> printfn "received: %s" m
+        | ReplyMessage channel -> channel.Reply("received ")
+        
+        return! messageLoop
+    }
+    messageLoop
+)
+
+[1..100]
+|> List.map (fun i -> async { agent.Post <| TxtMessage (i.ToString()) })
+|> Async.Parallel
+|> Async.RunSynchronously
+|> ignore
+
+agent.Post <| TxtMessage "moinsen"
+
+printfn "%s" <| agent.PostAndReply(ReplyMessage, 5000)
