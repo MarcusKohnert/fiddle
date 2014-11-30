695efaeb6b435a0347f743bb1ac3e8360db30bf4
Marcus Kohnert <marcus.dev@live.com>
2014-11-30 12:20:19 +0100

    get replies

--- FSharpAgents/FSharpAgents/Program.fs
+++ FSharpAgents/FSharpAgents/Program.fs
@@ -1,3 +1,16 @@
 ﻿[<EntryPoint>]
 let main argv = 
+
+    let reply(a:MailboxProcessor<tryOut.Message<'string>>) = 
+        printfn "replied %s" <| a.PostAndReply(tryOut.ReplyMessage, 5000)
+
+    let a = tryOut.GetAgent()
+    a.Post(tryOut.TxtMessage "Today")
+    reply a
+
+    a.Post(tryOut.TxtMessage "Tomorrow")
+    reply a
+    reply a
+
+    System.Console.ReadLine() |> ignore
     0
--- FSharpAgents/FSharpAgents/tryOut.fs
+++ FSharpAgents/FSharpAgents/tryOut.fs
@@ -1,27 +1,37 @@
 ﻿module tryOut
 
-type Message =
+type Message<'a> =
 | TxtMessage of string
-| ReplyMessage of AsyncReplyChannel<string>
+| ReplyMessage of AsyncReplyChannel<'a>
 
 let agent = MailboxProcessor.Start(fun inbox ->
-    let rec messageLoop = async {
-        let! msg = inbox.Receive()
-        match msg with
-        | TxtMessage m -> printfn "received: %s" m
-        | ReplyMessage channel -> channel.Reply("received ")
+    let rec messageLoop lastMsg = async {
         
-        return! messageLoop
+        let! msg = inbox.Receive()
+        printfn "ThreadId: %i" <| System.Threading.Thread.CurrentThread.ManagedThreadId
+        match msg with
+
+        | TxtMessage m -> 
+            printfn "received: %s" m
+            return! messageLoop m
+
+        | ReplyMessage channel -> 
+            channel.Reply lastMsg
+            return! messageLoop lastMsg
     }
-    messageLoop
+
+    messageLoop ""
 )
 
-[1..100]
-|> List.map (fun i -> async { agent.Post <| TxtMessage (i.ToString()) })
-|> Async.Parallel
-|> Async.RunSynchronously
-|> ignore
+let GetAgent() = agent
 
-agent.Post <| TxtMessage "moinsen"
-
-printfn "%s" <| agent.PostAndReply(ReplyMessage, 5000)
+let test() =
+    [1..100]
+    |> List.map (fun i -> async { agent.Post <| TxtMessage (i.ToString()) })
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+    
+    agent.Post <| TxtMessage "moinsen"
+    
+    printfn "replied %s" <| agent.PostAndReply(ReplyMessage, 5000)
