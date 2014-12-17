b96edc64e141231018c0502ef3991868b00dd1fa
Marcus Kohnert <marcus.dev@live.com>
2014-12-17 14:33:15 +0100

    Added controller and configured WebApi routes

---
+++ FSharpWebApi/FSharpWebApiOwin/Controller.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharpWebApiOwin
+
+open System.Web.Http
+
+type RequestController() =
+    inherit ApiController()
+
+    member this.Get() =
+        [0..10]
--- FSharpWebApi/FSharpWebApiOwin/Startup.fs
+++ FSharpWebApi/FSharpWebApiOwin/Startup.fs
@@ -2,19 +2,15 @@ namespace FSharpWebApiOwin
 
 open Owin
 open Microsoft.Owin
-//open System.Web.Http
+open System.Web.Http
 
 type Startup() =
     member this.Configuration(app:IAppBuilder) =
         
-//        let config = new HttpConfiguration();
-//        config.Routes.MapHttpRoute("default", "{controller}");
-//        app.UseWebApi(config);
-
-        app.Run(fun context ->
-            context.Response.ContentType = "text/plain" |> ignore
-            context.Response.WriteAsync("Hello World!")
-        )
+        let config = new HttpConfiguration()
+        config.Routes.MapHttpRoute("default", "{controller}") |> ignore
+        app.UseWebApi(config) |> ignore
+        ()
 
 module StartupModule =
     [<assembly:OwinStartup(typeof<Startup>)>]
