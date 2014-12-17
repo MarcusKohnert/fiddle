984d358c30efe9c9a0bdb9c852b64bd3eb4b8625
Marcus Kohnert <marcus.dev@live.com>
2014-12-17 14:01:08 +0100

    Removed PageInspector reference

--- FSharpWebApi/FSharpWebApiOwin/Startup.fs
+++ FSharpWebApi/FSharpWebApiOwin/Startup.fs
@@ -2,10 +2,15 @@ namespace FSharpWebApiOwin
 
 open Owin
 open Microsoft.Owin
+//open System.Web.Http
 
 type Startup() =
     member this.Configuration(app:IAppBuilder) =
         
+//        let config = new HttpConfiguration();
+//        config.Routes.MapHttpRoute("default", "{controller}");
+//        app.UseWebApi(config);
+
         app.Run(fun context ->
             context.Response.ContentType = "text/plain" |> ignore
             context.Response.WriteAsync("Hello World!")
