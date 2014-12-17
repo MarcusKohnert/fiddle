7aa2b05634d44dfbb618075bbb792a392e240b44
Marcus Kohnert <marcus.dev@live.com>
2014-12-17 14:40:17 +0100

    Use camel contract resolver

--- FSharpWebApi/FSharpWebApiOwin/Startup.fs
+++ FSharpWebApi/FSharpWebApiOwin/Startup.fs
@@ -8,8 +8,12 @@ type Startup() =
     member this.Configuration(app:IAppBuilder) =
         
         let config = new HttpConfiguration()
+        config.MapHttpAttributeRoutes()
         config.Routes.MapHttpRoute("default", "{controller}") |> ignore
         app.UseWebApi(config) |> ignore
+
+        config.Formatters.XmlFormatter.UseXmlSerializer <- true
+        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
         ()
 
 module StartupModule =
