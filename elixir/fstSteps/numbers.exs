c3123b732ca96cdc2002165f11067841fcfb34be
Marcus Kohnert <marcus.dev@live.com>
2015-02-13 12:05:31 +0100

    Guess solution

--- elixir/fstSteps/numbers.exs
+++ elixir/fstSteps/numbers.exs
@@ -1,7 +1,34 @@
 defmodule Numbers do
   def sum(0), do: 0
-  def sum(n), do: n + sum(n - 1)
+  def sum(n) when n > 0 do
+  	n + sum(n - 1)
+  end
 
   def gcd(x, 0), do: x
   def gcd(x, y), do: gcd(y, rem(x, y))
+
+  def guess(expected, range) do
+  	min .. max = range
+  	isIt(min, max, expected)
+  end
+
+  defp isIt(min, max, expected) when expected > min and expected < max do
+  	newmax = div((max - min), 2) + min
+  	echo(newmax)
+  	isIt(min, newmax, expected)
+  end
+
+  defp isIt(min, max, expected) when expected > max do
+  	newmax = div(max, 2) + max
+  	echo(newmax)
+  	isIt(max, newmax, expected)
+  end
+
+  defp isIt(min, max, expected) when expected === min or expected === max do
+  	IO.puts("#{expected}")
+  end
+
+  defp echo(guess), do: IO.puts("Is it #{guess}")
 end
+
+Numbers.guess(273, 1..1000)
