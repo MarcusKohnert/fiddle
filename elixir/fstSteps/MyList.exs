042ee22342f0b219af85e9fdde8fc1d018fefb24
Marcus Kohnert <marcus.dev@live.com>
2015-02-13 16:46:35 +0100

    Added MyList module

---
+++ elixir/fstSteps/MyList.exs
@@ -0,0 +1,12 @@
+defmodule MyList do
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+
+  def sum(list), do: _sum(list, 0)
+
+  defp _sum([], total), do: total
+  defp _sum([head | tail], total), do: _sum(tail, head + total)
+
+  def sum2([]), do: 0
+  def sum2([head | tail]), do: head + sum2(tail)
+end
