48a299e1d199063f0447c57d20e1b345945dff4a
Marcus Kohnert <marcus.dev@live.com>
2015-02-17 20:54:26 +0100

    Added max and ceasar

--- elixir/fstSteps/MyList.exs
+++ elixir/fstSteps/MyList.exs
@@ -9,4 +9,23 @@ defmodule MyList do
 
   def sum2([]), do: 0
   def sum2([head | tail]), do: head + sum2(tail)
+
+  def mapsum(list, func), do: map(list, func) |> sum
+
+  def max([a]), do: a
+  def max([a, b | []]) when a > b, do: a
+  def max([a, b | []]) when a <= b, do: b
+  def max([a, b | tail]) when a > b, do: max([a | tail])
+  def max([a, b | tail]) when a <= b, do: max([b | tail])
+
+  def ceasar([], _shift), do: ''
+  def ceasar([head | tail], shift) when head + shift > 122 do
+  	[head + shift - 26 | ceasar(tail, shift)]
+  end
+  def ceasar([head | tail], shift) do
+  	[head + shift | ceasar(tail, shift)]
+  end
 end
+
+MyList.ceasar('ryvkve', 13)
+|> IO.puts
