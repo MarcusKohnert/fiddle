606c9afa798744864b7ace7dbcbed268cc070309
Marcus Kohnert <marcus.dev@live.com>
2015-02-20 11:54:53 +0100

    CallerMember logging sample

---
+++ Logging/CallerAttributes/CallerAttributes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CallerAttributes
+{
+    class Program
+    {
+        private readonly ConsoleLogger logger;
+
+        static void Main(string[] args)
+        {
+            var p = new Program(new ConsoleLogger());
+            p.DoThis();
+            p.DoThat();
+
+            Console.WriteLine("[ENTER] to exit");
+            Console.ReadLine();
+        }
+
+        public Program(ConsoleLogger logger)
+        {
+            this.logger = logger;
+        }
+
+        private void DoThis()
+        {
+            this.logger.Log("I'm here");
+        }
+
+        private void DoThat()
+        {
+            this.logger.Log("I'm here");
+        }
+    }
+
+    internal class ConsoleLogger
+    {
+        internal void Log(string message, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
+        {
+            Console.WriteLine("{0}:\t{1}\t{2}", callerLineNumber, callerMemberName, message);
+        }
+    }
+}
