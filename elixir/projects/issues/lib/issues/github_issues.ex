674a0ca4337db14d49641b47e137fe5c85c9f769
Marcus Kohnert <marcus.dev@live.com>
2015-03-22 15:05:55 +0100

    Added sorting

--- elixir/projects/issues/lib/issues/github_issues.ex
+++ elixir/projects/issues/lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.Github do
   @user_agent [{"User-agent", "Elixir"}]	
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -7,11 +8,15 @@ defmodule Issues.Github do
     |> handle_response
   end
 
-  def issues_url(user, project), do: "https://api.github.com/repos/#{user}/#{project}/issues"
+  def issues_url(user, project), do: "#{@github_url}/repos/#{user}/#{project}/issues"
 
-  def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}), do: {:ok, body}
+  def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    {:ok, :jsx.decode(body)}
+  end
 
-  def handle_response({_, %HTTPoison.Response{status_code: ___, body: body}}), do: {:ok, body}
+  def handle_response({_, %HTTPoison.Response{status_code: ___, body: body}}) do
+    {:ok, :jsx.decode(body)}
+  end
 
   #def handle_response(%{status_code: 200, %HTTPoison.Response: body}), do: {:ok, body}
 
