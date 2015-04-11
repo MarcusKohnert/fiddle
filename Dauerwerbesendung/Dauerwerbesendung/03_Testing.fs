bc384f1e29781a9dded0858f9c8245901001dd8f
Marcus Kohnert <marcus.dev@live.com>
2015-04-11 12:00:23 +0200

    Test example

---
+++ Dauerwerbesendung/Dauerwerbesendung/03_Testing.fs
@@ -0,0 +1,16 @@
+﻿namespace Dauerwerbesendung
+
+open Xunit
+open Swensen.Unquote
+
+module Implementation =
+    let foo () = ""
+
+module Tests =
+
+    [<Fact>]
+    let ``Given a wonderful spec Calculate() returns the correct result`` () =
+        let expected = "-1"
+        let result = Implementation.foo()
+        test <@ expected = result @>
+        ()
