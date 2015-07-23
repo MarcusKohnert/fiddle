0dafdd9ec9004ad1fabd494945ec0108e7f4a668
Marcus Kohnert <marcus.dev@live.com>
2015-07-23 21:11:55 +0200

    Units of measure with conversion

---
+++ UnitsOfMeasure/UnitsOfMeasure/Conversions.fs
@@ -0,0 +1,14 @@
+﻿module Conversions
+
+    [<Measure>] type m
+
+    [<Measure>] 
+    type km =
+        static member inMetre = 1000<m/km>
+
+    let Do() =
+        let oneKm = 1<km>
+
+        let inM = oneKm * km.inMetre
+
+        ()
