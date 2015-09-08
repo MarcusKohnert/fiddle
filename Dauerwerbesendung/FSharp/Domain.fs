180f143db2d4404765dd717b834faca6f296db9a
Marcus Kohnert <marcus.dev@live.com>
2015-09-08 20:43:46 +0200

    Type inference

--- Dauerwerbesendung/FSharp/Domain.fs
+++ Dauerwerbesendung/FSharp/Domain.fs
@@ -1,3 +1,10 @@
 ﻿module Domain
 
+    // record types
+
     type Customer = { Firstname : string; Lastname : string }
+
+    // type inference
+
+    let GetFirstname customer =
+        customer.Firstname
