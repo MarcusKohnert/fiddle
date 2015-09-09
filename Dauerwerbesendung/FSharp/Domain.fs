772882e9b1293b02399be38b5a0f0eb61df88944
Marcus Kohnert <marcus.dev@live.com>
2015-09-09 17:52:27 +0200

    Discriminated union

--- Dauerwerbesendung/FSharp/Domain.fs
+++ Dauerwerbesendung/FSharp/Domain.fs
@@ -1,5 +1,8 @@
 ﻿module Domain
 
+    open System    
+    open System.Collections.Generic
+
     // record types
 
     type Customer = { Firstname : string; Lastname : string }
@@ -8,3 +11,13 @@
 
     let GetFirstname customer =
         customer.Firstname
+
+    type Zahlungsmittel =
+    | Bar of decimal
+    | Kreditkarte of int * DateTime
+    | Paypal of string
+
+    let DruckeRechnung(mittel:Zahlungsmittel) =
+        match mittel with
+        | Bar betrag -> sprintf "Bar: %A" betrag
+        | _ -> ""
