2967762dde3eb7885181751f5e577a855130bf98
Marcus Kohnert <marcus.dev@live.com>
2015-09-15 21:08:38 +0200

    Added script for showing basic type inference

--- Dauerwerbesendung/FSharp/Script.fsx
+++ Dauerwerbesendung/FSharp/Script.fsx
@@ -1,6 +1,24 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-//#load "Library1.fs"
 open FSharp
+open System
 
+let someInt = 32
+
+let someString = "Hello World"
+
+let someOtherString : string = "Hello FSharp"
+
+let someDecimal = 12m
+
+// let someDecimal : decimal = 12.0m
+
+let someFunction () =
+    42
+
+let sayHello name =
+    sprintf "Hello %s" name
+
+let callSplit(value:string) =
+    value.Split('#')
