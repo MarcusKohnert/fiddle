b5c17842fd3cb4ad033be1d39919655fd61078d3
Marcus Kohnert <marcus.dev@live.com>
2015-09-16 08:39:50 +0200

    Equality test

--- Dauerwerbesendung/FSharp/Tests.fs
+++ Dauerwerbesendung/FSharp/Tests.fs
@@ -2,6 +2,7 @@
 
     // Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
     open Microsoft.VisualStudio.TestTools.UnitTesting
+    open Domain
 
     [<TestClass>]
     type ``Given a customer``() =
@@ -9,3 +10,17 @@
         [<TestMethod>]
         member __.``Then it's persited in the database``() =
             ()
+
+        [<TestMethod>]
+        member __.``compared to another is equal``() =
+            let first = { Firstname = "Marcus"; Lastname = "Kohnert" }
+            let second = { Firstname = "Marcus"; Lastname = "Kohnert" }
+
+            Assert.AreEqual(first, second)
+
+        [<TestMethod>]
+        member __.``compared to another is not equal``() =
+            let first = { Firstname = "Marcus"; Lastname = "Kohnert" }
+            let second = { Firstname = "Marcus2"; Lastname = "Kohnert" }
+
+            Assert.AreNotEqual(first, second)
