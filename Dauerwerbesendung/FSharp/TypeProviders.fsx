18d0eadb86aa0cc9ec0d7e1b6ca865be5cab10e6
Marcus Kohnert <marcus.dev@live.com>
2015-09-18 19:12:32 +0200

    Added typeprovider sample

---
+++ Dauerwerbesendung/FSharp/TypeProviders.fsx
@@ -0,0 +1,11 @@
+﻿#r @"D:\MKK\src\fiddle\Dauerwerbesendung\packages\FSharp.Data.2.2.5\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+type SaxSysRepos = JsonProvider<"https://api.github.com/users/saxsys/repos">
+SaxSysRepos.GetSamples()
+|> Seq.map (fun r -> r.Name)
+|> Seq.iter (fun n -> printfn "%s" n)
+
+let worldbank = WorldBankData.GetDataContext()
+worldbank.Countries.Germany.Indicators.``Population (Total)``.Item(2014)
