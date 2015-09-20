82f020c8573511e69b76f89e8703b61000bfa475
Marcus Kohnert <marcus.dev@live.com>
2015-09-20 17:35:44 +0200

    Added units of measure sample

---
+++ Dauerwerbesendung/FSharp/UnitsOfMeasure.fs
@@ -0,0 +1,15 @@
+﻿module UnitsOfMeasure
+
+    [<Measure>] type km
+    [<Measure>] type h
+
+    let Do() =
+        
+        let strecke = 5.0<km>
+        let zeit = 1.0<h>
+
+        let geschwindigkeit = strecke / zeit
+
+//        let foo = geschwindigkeit + strecke
+
+        ()
